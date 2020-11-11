48caf6a1696efb24aaa0df5ac9d8753769ac2d7e
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-12 08:57:55 +0900

    Revert "Revert "ufb_manufacture_serial①""
    This reverts commit 93cc8490ae325d82c35056ff7e780c27d2bc817f.

--- UFBmanagement/ufb_manufacture_serial.js
+++ UFBmanagement/ufb_manufacture_serial.js
@@ -4,34 +4,34 @@
   function autoNum(event) {
       var record = event.record;
 
-      // 日付を取得し、2桁の年を取得する
-      var dt = record['日付'].value;
-      var dtyy = dt.substring(0, 4); // ll dtyy は その日にする
-      var dtmin = dtyy + '-01-01';  // ll dtmin は　その日の00:00
-      var dtmax = (parseInt(dtyy, 10) + 1) + '-01-01'; // ll dtmax は その日の23:59
+      // 製造終了日時を取得し、2桁の年を取得する
+      var dt = record['製造終了日時'].value;
+      var dtyymmdd = dt.substring(0, 10); // ll 製造終了日時の日付部分
+      var dtmin = dtyymmdd + ' 00:00'; // ll dtyymmddの00:00
+      var dtmax = (parseInt(dtyymmdd, 10) + 1) + ' 00:00'; // ll dtyymmddの翌日の00:00
 
       // クエリ文の設定
       var query = {
           "app": kintone.app.getId(),
-          "query": '日付 >= "' + dtmin + '" and 日付 < "' + dtmax + '" order by 自動採番 desc limit 1'  // ll < を　<= にする
+          "query": '製造終了日時 >= "' + dtmin + '" and 製造終了日時 < "' + dtmax + '" order by 製造番号 desc limit 1'
       };
 
-      // 設定された日付から最新の番号を取得する
+      // 設定された製造終了日時から最新の番号を取得する
       return kintone.api(kintone.api.url('/k/v1/records', true), 'GET', query).then(function(resp) {
           var records = resp.records;
 
           // 対象レコードがあった場合
           if (records.length > 0) {
               var rec = records[0];
-              var autono = rec['自動採番'].value;
+              var autono = rec['製造番号'].value;
               autono = parseInt(autono.substring(3), 10) + 1;
               autono = '00000' + autono;
               autono = dt.substring(2, 4) + '-' + autono.substring(autono.length - 5);
-              event.record['自動採番'].value = autono;
+              event.record['製造番号'].value = autono;
 
           // 対象レコードがなかった場合
           } else {
-              event.record['自動採番'].value = dt.substring(2, 4) + '-00001';
+              event.record['製造番号'].value = dt.substring(2, 4) + '-00001';
           }
           return event;
       }).catch(function(e) {
@@ -48,7 +48,7 @@
   kintone.events.on('app.record.create.show', function(event) {
       var record = event.record;
       //フィールドを非活性にする
-      record['自動採番'].disabled = true;
+      record['製造番号'].disabled = true;
       return event;
   });
 
@@ -57,8 +57,8 @@
   kintone.events.on(['app.record.edit.show', 'app.record.index.edit.show'], function(event) {
       var record = event.record;
       //フィールドを非活性にする
-      record['自動採番'].disabled = true;
-      record['日付'].disabled = true;
+      record['製造番号'].disabled = true;
+      record['製造終了日時'].disabled = true;
       return event;
   });
 
