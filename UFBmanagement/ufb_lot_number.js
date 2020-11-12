056e92bdba3207b4525b68390a1ea841253641eb
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-12 12:41:25 +0900

    ロットNo.に修正

--- UFBmanagement/ufb_lot_number.js
+++ UFBmanagement/ufb_lot_number.js
@@ -4,34 +4,31 @@
   function autoNum(event) {
       var record = event.record;
 
-      // 製造開始日時を取得し、2桁の年を取得する
-      var dt = record['製造開始日時'].value;
-      var dtyymmdd = dt.substring(0, 10); // ll 製造開始日時の日付部分
-      var dtmin = dtyymmdd; // ll dtyymmdd
-      var dtmax = dt.substring(0, 8) + (parseInt(dt.substring(8,10), 10) + 1); // ll dtyymmddの翌日
+      // 製造番号選択を取得し、2桁の年を取得する
+      var dt = record['製造番号選択'].value;
 
       // クエリ文の設定
       var query = {
           "app": kintone.app.getId(),
-          "query": '製造開始日時 >= "' + dtmin + '" and 製造開始日時 < "' + dtmax + '" order by 製造番号 desc limit 1'
+          "query": '製造番号選択 = "' + dt + '" order by ロットNo desc limit 1'
       };
 
-      // 設定された製造開始日時から最新の番号を取得する
+      // 設定された製造番号選択から最新の番号を取得する
       return kintone.api(kintone.api.url('/k/v1/records', true), 'GET', query).then(function(resp) {
           var records = resp.records;
 
           // 対象レコードがあった場合
           if (records.length > 0) {
               var rec = records[0];
-              var autono = rec['製造番号'].value;
-              autono = parseInt(autono.substring(7, 9), 10) + 1;
+              var autono = rec['ロットNo'].value;
+              autono = parseInt(autono.substring(10, 12), 10) + 1;
               autono = '00' + autono;
-              autono = dt.substring(2, 4) + dt.substring(5, 7) + dt.substring(8, 10) + '-' + autono.substring(autono.length - 2);
-              event.record['製造番号'].value = autono;
+              autono = dt.substring(0, 9) + '-' + autono.substring(autono.length - 2);
+              event.record['ロットNo'].value = autono;
 
           // 対象レコードがなかった場合
           } else {
-              event.record['製造番号'].value = dt.substring(2, 4) + dt.substring(5, 7) + dt.substring(8, 10) + '-01';
+              event.record['ロットNo'].value = dt.substring(0, 9) + '-01';
           }
           return event;
       }).catch(function(e) {
@@ -48,7 +45,7 @@
   kintone.events.on('app.record.create.show', function(event) {
       var record = event.record;
       //フィールドを非活性にする
-      record['製造番号'].disabled = true;
+      record['ロットNo'].disabled = true;
       return event;
   });
 
@@ -57,8 +54,8 @@
   kintone.events.on(['app.record.edit.show', 'app.record.index.edit.show'], function(event) {
       var record = event.record;
       //フィールドを非活性にする
-      record['製造番号'].disabled = true;
-      record['製造開始日時'].disabled = true;
+      record['ロットNo'].disabled = true;
+      record['製造番号選択'].disabled = true;
       return event;
   });
 
