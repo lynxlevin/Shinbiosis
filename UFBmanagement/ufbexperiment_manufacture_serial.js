c4b468b644610e57dc09b17f73a54763f0438a52
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-12 15:40:11 +0900

    参照先をルックアップから文字列に変更

--- UFBmanagement/ufbexperiment_manufacture_serial.js
+++ UFBmanagement/ufbexperiment_manufacture_serial.js
@@ -4,8 +4,8 @@
   function lotToManufacture(event) {
       var record = event.record;
 
-      // 「使用したロットNo」を取得し、「製造番号」(dtm)にする
-      var dt = record['使用したロットNo'].value;
+      // 「使用したロットNo参照用」を取得し、「製造番号」(dtm)にする
+      var dt = record['使用したロットNo参照用'].value;
       var dtm = dt.substring(0,9);
 
       // 「製造番号参照用」にdtmを表示する
@@ -13,8 +13,8 @@
       return event;
   }
 
-  // 新規作成画面と編集画面で「使用したロットNo.」を取得したときに動作する
-  kintone.events.on(['app.record.create.change.使用したロットNo', 'app.record.edit.change.使用したロットNo'], lotToManufacture);
+  // 新規作成画面と編集画面で「使用したロットNo参照用.」を取得したときに動作する
+  kintone.events.on(['app.record.create.change.使用したロットNo参照用', 'app.record.edit.change.使用したロットNo参照用'], lotToManufacture);
 
 
   // 新規作成画面、編集画面表示
